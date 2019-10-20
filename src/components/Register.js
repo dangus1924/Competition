e50dcf169d3a2bf710ddd44f8c29800f545f80ad
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-19 22:24:01 -0400

    install axios

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import { withFormik } from 'formik';
+import * as yup from 'yup';
 
-
-function Register() {
-    return <h1>Sign Up</h1>
+function Register(props) {
+    const {touched, errors, status, handleSubmit, handleChange} = props;
+    return(
+        
+        <form onSubmit={handleSubmit}>
+            <h1>Join </h1>
+            
+        </form>
+    )
 }
 
-export default Register;
+export default withFormik({
+    mapPropsToValues: (values) =>{
+    return {
+        name: values.name || '',
+        email: values.email || '',
+        password: values.password || '',
+        agreement: values.agreement || false
+
+    }
+},
+validationSchema: yup.object().shape({
+    name: yup.string().required('name is required'),
+    email: yup.string().email().required('Email is required'),
+    password: yup.string().required('Password is required'),
+    agreement: yup.boolean().oneOf([true], 'You must agree with our terms and service'),
+}),
+handleSubmit: (values, {setStatus}) => {
+    axios.post('https://reqres.in/api/users',values)
+    .then((res) => {
+        
+        setStatus(res.data)
+    })
+    .catch((err) => {
+        console.log('Error:', err)
+    })
+   
+},
+
+})(Register)
