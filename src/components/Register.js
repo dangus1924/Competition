3aa70e928eebe1b714bda54cfbf67ed4231d8991
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-19 23:16:29 -0400

    add a register form with formik and used a temp axios call for users

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,36 +1,85 @@
-import React from 'react';
-import { withFormik } from 'formik';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
+import axios from 'axios';
+import { Button } from '@material-ui/core'
 
 function Register(props) {
-    const {touched, errors, status, handleSubmit, handleChange} = props;
+    const {touched, errors, status, handleSubmit} = props;
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status])
+        }
+    },[status])
     return(
         
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
             <h1>Join </h1>
+            {touched.firstName && errors.FirstName && <p className="error">{errors.FirstName}</p>}
+            <Field type="text" 
+                    name="firstName"   
+                    placeholder="First Name" />
+
+            {touched.lastName && errors.lastName && <p className="error">{errors.lastName}</p>}
+            <Field type="text" 
+                    name="lastName" 
+                    placeholder="Last Name" />
+
+            {touched.email && errors.email && <p className="error">{errors.email}</p>}
+            <Field type="email" 
+                    name="email" 
+                    placeholder="Email" />
+
+            {touched.phone && errors.phone && <p className="error">{errors.phone}</p>}
+            <Field type="text" 
+                    name="phone" 
+                    placeholder="Cell Phone" />
+
+            {touched.textConfirm && errors.textConfirm && <p className="error">{errors.textConfirm}</p>}
+            <Field name="textConfirm" 
+                    component="select" 
+                    placeholder="Can you recieve text">
+                    <option value="" disabled>Can you recieve text?</option> 
+                    <option value="yes">Yes</option>
+                    <option value="no">No</option>                     
+            </Field>
+
+            <Button type="submit">Submit</Button>
+
+            {users.map((user) => {
+                return <div>Welcome: {user.name} </div>
+            })}
             
-        </form>
+        </Form>
     )
 }
 
 export default withFormik({
     mapPropsToValues: (values) =>{
     return {
-        name: values.name || '',
+        firstName: values.firstName || '',
+        lastName: values.lastName || '',
+        phone: values.phone || '',
         email: values.email || '',
         password: values.password || '',
-        agreement: values.agreement || false
+        agreement: values.agreement || false,
+        textConfirm: values.textConfirm,
 
     }
 },
 validationSchema: yup.object().shape({
-    name: yup.string().required('name is required'),
+    firstName: yup.string().required('First name is required'),
+    lastName: yup.string().required('First name is required'),
+    phone: yup.string().required('Mobile number is required'),
     email: yup.string().email().required('Email is required'),
     password: yup.string().required('Password is required'),
     agreement: yup.boolean().oneOf([true], 'You must agree with our terms and service'),
+    textConfirm: yup.boolean().oneOf([true], 'Please select an option'),
 }),
 handleSubmit: (values, {setStatus}) => {
     axios.post('https://reqres.in/api/users',values)
+    
     .then((res) => {
         
         setStatus(res.data)
